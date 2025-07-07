1833267007906f9cfcb9ca01650cfd5ee50ecf54
Shelton Louis <louisshelton0@gmail.com>
2025-07-07 18:08:48 -0400

    fix: Use version in download URLs
    Use the `version` variable in the download URLs to make them dynamic.

--- Casks/javascript-package-delegator.rb
+++ Casks/javascript-package-delegator.rb
@@ -15,22 +15,22 @@ cask "javascript-package-delegator" do
   on_macos do
     on_intel do
       sha256 "e32f287b34ae94d7bbbe3c4963f34180668ef993c97a1b8c9068847e2bc18443"
-      url "https://github.com/louiss0/javascript-package-delegator/releases/download/v1.0.0/javascript-package-delegator_1.0.0_darwin_amd64.tar.gz"
+      url "https://github.com/louiss0/javascript-package-delegator/releases/download/v#{version}/javascript-package-delegator_#{version}_darwin_amd64.tar.gz"
     end
     on_arm do
       sha256 "be33e3710900e08a90b541daf830ed6bfca781e2ce71c0076470c96552f65aa6"
-      url "https://github.com/louiss0/javascript-package-delegator/releases/download/v1.0.0/javascript-package-delegator_1.0.0_darwin_arm64.tar.gz"
+      url "https://github.com/louiss0/javascript-package-delegator/releases/download/v#{version}/javascript-package-delegator_#{version}_darwin_arm64.tar.gz"
     end
   end
 
   on_linux do
     on_intel do
       sha256 "76a68a7480ea7d956ada41f003716b525564017f36f22b4b42b50d66f6d426ce"
-      url "https://github.com/louiss0/javascript-package-delegator/releases/download/v1.0.0/javascript-package-delegator_1.0.0_linux_amd64.tar.gz"
+      url "https://github.com/louiss0/javascript-package-delegator/releases/download/v#{version}/javascript-package-delegator_#{version}_linux_amd64.tar.gz"
     end
     on_arm do
       sha256 "15380d62cbad6938fcdbf5e66ae039eb0c3d4d743a06205937aa2661a3eb2d3f"
-      url "https://github.com/louiss0/javascript-package-delegator/releases/download/v1.0.0/javascript-package-delegator_1.0.0_linux_arm64.tar.gz"
+      url "https://github.com/louiss0/javascript-package-delegator/releases/download/v#{version}/javascript-package-delegator_#{version}_linux_arm64.tar.gz"
     end
   end
 
